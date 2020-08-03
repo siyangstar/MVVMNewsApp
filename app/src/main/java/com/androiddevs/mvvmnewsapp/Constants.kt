940bf629c5caff9645b7cafdee4b63d746fc452a
18580683725 <819879>
2020-08-03 18:22:23 +0800

    update

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/Constants.kt
@@ -0,0 +1,9 @@
+package com.androiddevs.mvvmnewsapp
+
+class Constants {
+    companion object {
+        const val API_KEY = "9f69d5e818204f31b6c6e2cdc300392a"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+    }
+}
---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/reporitory/NewsRepository.kt
@@ -0,0 +1,16 @@
+package com.androiddevs.mvvmnewsapp.reporitory
+
+import android.app.DownloadManager
+import com.androiddevs.mvvmnewsapp.api.RetrofitInstance
+import com.androiddevs.mvvmnewsapp.db.ArticleDatabase
+
+class NewsRepository(
+    val db: ArticleDatabase
+) {
+
+    suspend fun getBreakingNews(countryCode: String, pageNumber: Int) =
+        RetrofitInstance.api.getBreakingNews(countryCode, pageNumber)
+
+    suspend fun searchNews(searchQuery: String, pageNumber: Int) =
+        RetrofitInstance.api.searchForNews(searchQuery, pageNumber)
+}
